9256910f7fcef06020eb98bb4941019d92f746ac
lhy19961016 <55981075+lhy19961016@users.noreply.github.com>
2019-11-23 17:15:14 +0300

    Create Web_Client.py

---
+++ WEB/Web_Client.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+import os
+import pickle
+import time
+
+URL_STAT = 'http://localhost:9527/STAT'
+URL_ENTI = 'http://localhost:9527/ENTI'
+
+
+def File_Reader(file_R, num):
+    Reader = pd.read_csv(file_R, sep=';', encoding='iso-8859-1')
+    data_R = Reader.iloc[0:num, :]
+    Pack_Data = pickle.dumps(data_R)
+    return Pack_Data
+
+
+Num = int(input('Enter the quantity of the tweets:'))
+print('Enter the path of the file:')
+File_Path = input()
+
+Data_Ready_For_Sending = File_Reader(File_Path, Num)
+Size_data = os.path.getsize(File_Path)
+if int(Size_data) == 0 and int(Size_data) < 0:
+    print("error")
+else:
+    print("Verification completed")
+
+Command = input('Enter the command:')
+if Command != 'STAT' and Command != 'ENTI':
+    print('Your Command is not in the list of Command,Please enter it again')
+    Command = input('enter again:')
+
+if Command == 'STAT':
+    rst = requests.request("POST", URL_STAT, data=Data_Ready_For_Sending)
+if Command == 'ENTI':
+    rst = requests.request("POST", URL_ENTI, data=Data_Ready_For_Sending)
+
+time.sleep(2)
+Get_Data = pickle.loads(rst.content)
+print("please enter the path of the file for saving Getting data")
+File_Getting_Path = input()
+Get_Data.to_csv(File_Getting_Path, sep=';', encoding='iso-8859-1')
