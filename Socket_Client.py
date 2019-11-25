02a3c361fac850364598d29d43d7fad5e565bde0
lhy19961016 <55981075+lhy19961016@users.noreply.github.com>
2019-11-25 03:18:17 +0300

    Update Socket_Client.py

--- Socket_Client.py
+++ Socket_Client.py
@@ -8,7 +8,8 @@ import os
 
 def File_Reader(file_R):
     Reader = pd.read_csv(file_R, sep=';', encoding='iso-8859-1')
-    Pack_Data = pickle.dumps(Reader)
+    Reader_1 = Reader.iloc[0:7, ]
+    Pack_Data = pickle.dumps(Reader_1)
     return Pack_Data
 
 
@@ -41,20 +42,21 @@ while True:
     # ----------------Send the Command and Size_File-------e.g:"STAT9999999(999...is size of the file
     client.send((request + str(Size_File_Byte)).encode('utf-8'))  # 1->
     # ---------------------From server client get the"Get the size and command"-------------
-    received_data_size = client.recv(4096)  # 2<-
+    received_MSG_1 = client.recv(4096)  # 2<-
+    print(received_MSG_1.decode())
     # ------------------------Client send the data to Server for processing-----
     client.send(File_Byte)  # 3->
     # ---------------------Get the Size of Returned file and the file after pickling-------------------
-    Size_Returned_File = int(client.recv(4096))  # 4<-
+    Size_Returned_File = client.recv(4096)  # 4<-
     Count_Rest_Data = 0
     Data_Get = []
     # -------We have made sure about the files need to STAT or ENTI on server,so there is only one situation
-    while Count_Rest_Data < Size_Returned_File:
+    while Count_Rest_Data < int(Size_Returned_File.decode()):
         Data_Returned = client.recv(4096)  # 5<-
         Data_Get.append(Data_Returned)
         Data_Get += Data_Returned
 
     Data_Returned_Unpickle = pickle.loads(b"".join(Data_Get))
-    Data_Returned_Unpickle.to_csv(r'G:/' + request + '_data.csv', encoding='iso-8859-1')
+    Data_Returned_Unpickle.to_csv(r'./' + request + '_data.csv', encoding='iso-8859-1')
 
     client.close()
